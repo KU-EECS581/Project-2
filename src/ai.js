894d9f56ce09999eef3cb205cef2cb77e9a78ae9
Riley Meyerkorth <117927480+m-riley04@users.noreply.github.com>
2025-09-30 14:48:16 -0500

    add ai class outline

---
+++ src/ai.js
@@ -0,0 +1,34 @@
+/**
+ * Name: ai.js
+ * Description: Contains all the JavaScript code for the AI player in the game of minesweeper.
+ * Original Author(s): Riley Meyerkorth
+ * Dependencies: constants.js utils.js ui.js
+ * Date Revised: 10 September 2025
+ */
+
+class AI {
+    constructor(game, difficulty) {
+        this.game = game;
+        this.difficulty = difficulty;
+    }
+
+    /**
+     * Makes a move for the AI player.
+     */
+    makeMove() {
+        // AI logic to make a move
+
+    }
+
+    _easyMove() {
+        // Easy difficulty AI logic
+    }
+
+    _mediumMove() {
+        // Medium difficulty AI logic
+    }
+
+    _hardMove() {
+        // Hard difficulty AI logic
+    }
+}
